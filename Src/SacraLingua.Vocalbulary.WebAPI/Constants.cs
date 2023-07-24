6ed482f0525da511075e330a36554693e91cc6bc
kamil.michal.ksiazek@gmail.com <kamil.michal.ksiazek@gmail.com>
2023-07-24 20:40:04 +0200

    Setup Controller

---
+++ Src/SacraLingua.Vocalbulary.WebAPI/Constants.cs
@@ -0,0 +1,18 @@
+﻿namespace SacraLingua.Vocalbulary.WebAPI
+{
+    /// <summary>
+    /// Constants value for Web API
+    /// </summary>
+    public static class Constants
+    {
+        /// <summary>
+        /// Base API Route
+        /// </summary>
+        public const string BaseRoute = "api/v{version}";
+        public const string ApiVersion = "1";
+        /// <summary>
+        /// Users Route
+        /// </summary>
+        public const string UserRoute = $"{BaseRoute}/users/";
+    }
+}
---
+++ Src/SacraLingua.Vocalbulary.WebAPI/Controllers/UsersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SacraLingua.Vocalbulary.WebAPI.Controllers
+{
+    [Route(Constants.UserRoute)]
+    [ApiVersion(Constants.ApiVersion)]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        
+    }
+}
