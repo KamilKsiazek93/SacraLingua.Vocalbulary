d9cf1a18e318a54cf02b5557c7e88d954640a3f7
kamil.michal.ksiazek@gmail.com <kamil.michal.ksiazek@gmail.com>
2023-10-16 21:03:44 +0200

    Updated domain exception

---
+++ Src/SacraLingua.Vocalbulary.Domain/Exceptions/DomainValidationRulesException.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SacraLingua.Vocalbulary.Domain.Exceptions
+{
+    public class DomainValidationRulesException : Exception
+    {
+        public ICollection<string> ErrorMessages { get; }
+
+        public DomainValidationRulesException(ICollection<ValidationResult> brokenRules, string message)
+        : base(message)
+        {
+            ErrorMessages = brokenRules.Select(x => x.ErrorMessage).ToList();
+        }
+
+        public override string Message
+        {
+            get
+            {
+                if (ErrorMessages.Any())
+                {
+                    return $"{base.Message}{Environment.NewLine} {string.Join(Environment.NewLine, ErrorMessages)}{Environment.NewLine}";
+                }
+                return base.Message;
+            }
+        }
+    }
+}
